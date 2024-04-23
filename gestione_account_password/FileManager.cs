d43b8aacdcc139b2374a9138bdcb7246795a8df7
Iannelli.20192 <Iannelli.20192@E2-9-PC06>
2024-04-23 13:44:10 +0200

    implementazione thread-safe del pattern singleton

---
+++ gestione_account_password/FileManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestione_account_password
+{
+    public sealed class FileManager
+    {
+        private static FileManager instance = null;
+        private static readonly object padlock = new object();
+
+        private FileManager()
+        {
+
+        }
+
+        public static FileManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (padlock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new FileManager();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+}
