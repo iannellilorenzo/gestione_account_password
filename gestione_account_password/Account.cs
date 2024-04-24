78addda16bc28e365f725338b5bf4fb8d0aaf8ac
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-04-24 17:59:58 +0200

    implementata la classe Account

---
+++ gestione_account_password/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestione_account_password
+{
+    public class Account
+    {
+        private string _name;
+        private string _email;
+        private PasswordManager _password;
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+
+        public PasswordManager Password
+        {
+            get => _password;
+            set => _password = value;
+        }
+
+        public Account()
+        {
+            Name = "";
+            Email = "";
+            Password = new("");
+        }
+
+        public Account(string name, string email, PasswordManager password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+}
